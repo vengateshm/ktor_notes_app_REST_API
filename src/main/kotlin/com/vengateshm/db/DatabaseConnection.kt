7c0c514fb2d9199343b67f0af48a1f21559550f3
Vengatesh M <vengateshm.13@gmail.com>
2021-11-09 12:41:00 +0530

    db url added

--- src/main/kotlin/com/vengateshm/db/DatabaseConnection.kt
+++ src/main/kotlin/com/vengateshm/db/DatabaseConnection.kt
@@ -4,9 +4,9 @@ import org.ktorm.database.Database
 
 object DatabaseConnection {
     val database = Database.connect(
-        url = "jdbc:postgresql://ec2-34-203-182-172.compute-1.amazonaws.com:5432/dfmchkkfocl56e",
+        url = "jdbc:postgresql://ec2-3-220-90-40.compute-1.amazonaws.com:5432/d8qpvq9u0kej1h",
         driver = "org.postgresql.Driver",
-        user = "veepddedhnjvcu",
-        password = "6f7f829f13eeda204e7134645014a640755c58cbf65a6539acfb87c29163558d"
+        user = "wbgvkrnfjdnpkc",
+        password = "07fb1b95f8f3d2e7446c0db1cd58d759f45723f61ba023081f482fc2f0473bbe"
     )
 }
