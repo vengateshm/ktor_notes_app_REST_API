0f6b01fdb2197710e43edf9c8923c8c235d5e7ab
Vengatesh M <vengateshm.13@gmail.com>
2021-11-10 18:39:11 +0530

    db attributes read from env variables

--- src/main/kotlin/com/vengateshm/db/DatabaseConnection.kt
+++ src/main/kotlin/com/vengateshm/db/DatabaseConnection.kt
@@ -4,9 +4,9 @@ import org.ktorm.database.Database
 
 object DatabaseConnection {
     val database = Database.connect(
-        url = "jdbc:postgresql://ec2-3-220-90-40.compute-1.amazonaws.com:5432/d8qpvq9u0kej1h",
-        driver = "org.postgresql.Driver",
-        user = "wbgvkrnfjdnpkc",
-        password = "07fb1b95f8f3d2e7446c0db1cd58d759f45723f61ba023081f482fc2f0473bbe"
+        url = System.getenv("DB_URL"),
+        driver = System.getenv("DB_DRIVER"),
+        user = System.getenv("DB_USERNAME"),
+        password = System.getenv("DB_PASSWORD")
     )
 }
